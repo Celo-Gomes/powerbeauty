f6e45ade08b37602d9799a638daeafaf03a705fb
Marcelo Gomes <marcelog.3212@gmail.com>
2024-09-13 17:15:54 -0300

    test effect004

--- script.js
+++ script.js
@@ -30,24 +30,30 @@ button.onclick=()=>{
 // window.onload = checkEmailSent;
 
 
-    // Função para verificar se o e-mail foi enviado
-    function checkEmailSent() {
-        const params = new URLSearchParams(window.location.search);
-        const emailSent = params.get('email'); // Verifica se o parâmetro 'email' está presente
-        
-        // Exibe a mensagem somente se o parâmetro 'email=enviado' estiver na URL
-        if (emailSent === 'enviado') {
-            const message = document.getElementById('success-message');
-            message.style.display = 'block'; // Exibe a mensagem
 
-            // Remove a mensagem após 5 segundos
-            setTimeout(() => {
-                message.style.display = 'none';
-            }, 5000);
-        }
+// Função para verificar se o e-mail foi enviado
+function checkEmailSent() {
+    const params = new URLSearchParams(window.location.search);
+    const emailSent = params.get('email'); // Verifica se o parâmetro 'email' está presente
+
+    // Exibe a mensagem somente se o parâmetro 'email=enviado' estiver na URL
+    if (emailSent === 'enviado') {
+        const message = document.getElementById('success-message');
+        message.style.display = 'block'; // Exibe a mensagem
+
+        // Remove a mensagem após 5 segundos
+        setTimeout(() => {
+            message.style.display = 'none';
+        }, 5000);
+
+        // Remove o parâmetro 'email' da URL sem recarregar a página
+        const newUrl = window.location.href.split('?')[0];
+        window.history.replaceState({}, document.title, newUrl);
     }
+}
 
-    // Chama a função quando a página é carregada
-    window.onload = checkEmailSent;
+// Chama a função quando a página é carregada
+window.onload = checkEmailSent;
+
 
 
