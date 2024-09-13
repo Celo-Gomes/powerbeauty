b57a1d8a8cb3ff5c4213d2d28ac1ec2deac69f3b
Marcelo Gomes <marcelog.3212@gmail.com>
2024-09-13 16:30:30 -0300

    teste effect001

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ button.onclick=()=>{
     } else{
         photo.setAttribute('src', './assets/avatar_night1.png')
     }
-}       
+}  
+
+
+function checkEmailSent() {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('email') === 'enviado') {
+        const message = document.getElementById('success-message');
+        message.style.display = 'block'; // Exibe a mensagem
+
+        // Remove a mensagem após 5 segundos
+        setTimeout(() => {
+            message.style.display = 'none';
+        }, 5000);
+    }
+}
+
+// Chama a função quando a página é carregada
+window.onload = checkEmailSent;
 
 
